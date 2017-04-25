6ee32afb10b36e3f3305876c5dd0e5011f8a2b6c
kimberlylo <lokimberly22@gmail.com>
2017-04-24 17:37:29 -0700

    MyJSON implementation

--- src/com/google/codeu/codingchallenge/MyJSON.java
+++ src/com/google/codeu/codingchallenge/MyJSON.java
@@ -18,37 +18,150 @@ import java.util.Collection;
 
 final class MyJSON implements JSON {
 
-  @Override
-  public JSON getObject(String name) {
-    // TODO: implement this
-    return null;
-  }
+	private String key;
+	private String val;
+	private String[] keyList = new String[2];
+	private String[] valList = new String[2];
+	private JSON innerJSON;
+	private Collection<String> objNames;
+	private Collection<String> stringNames;
 
-  @Override
-  public JSON setObject(String name, JSON value) {
-    // TODO: implement this
-    return this;
-  }
+	// case 1: {}
+	public MyJSON() {
+		key = null;
+		val = null;
+		innerJSON = null;
+		objNames = null;
+		stringNames = null;
+	}
 
-  @Override
-  public String getString(String name) {
-    // TODO: implement this
-    return null;
-  }
+	// case 2: {"key":"val"}
+	public MyJSON(String jKey, String jVal) {
+		key = jKey;
+		val = jVal;
+		innerJSON = null;
+		objNames = null;
+		stringNames = null;
+	}
 
-  @Override
-  public JSON setString(String name, String value) {
-    // TODO: implement this
-    return this;
-  }
+	// {"key":"val", "key":"val"}
+	public MyJSON(String[] jKey, String[] jVal) {
+		keyList = jKey;
+		valList = jVal;
+		key = null;
+		val = null;
+		innerJSON = null;
+		objNames = null;
+		stringNames = null;
+	}
 
-  @Override
-  public void getObjects(Collection<String> names) {
-    // TODO: implement this
-  }
+	// case 3: {"key1":{"key2":"val2","key3":"val3"}}
+	public MyJSON(String jKey, JSON jsonObj) {
+		key = jKey;
+		val = null;
+		innerJSON = jsonObj;
+		objNames = null;
+		stringNames = null;
+	}
 
-  @Override
-  public void getStrings(Collection<String> names) {
-    // TODO: implement this
-  }
+	@Override
+	public JSON getObject(String name) {
+		// TODO: implement this
+		// Get the value of the nested object with the given name. If there is
+		// no nested object with that name, the method will return null.
+		if (key != null && name.equals(key)) {
+			return innerJSON;
+		}
+		return null;
+	}
+
+	@Override
+	public JSON setObject(String name, JSON value) {
+		// TODO: implement this
+		// Set the value of the nested object with the given name. Any old value
+		// should be overwritten. This method will always return a reference to
+		// "this".
+		if (key != null && name.equals(key)) {
+			innerJSON = value;
+		}
+		return this;
+	}
+
+	@Override
+	public String getString(String name) {
+		// TODO: implement this
+		// Get the string value within this object that has the given name. if
+		// there is no string with the given name, the method will return null.
+
+		if (key != null && name.equals(key)) {
+			return val;
+		}
+
+		if (keyList[0] != null) {
+			if (keyList[0].equals(name)) {
+				return valList[0];
+			} else if (keyList[1].equals(name)) {
+				return valList[1];
+			} else {
+				return null;
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public JSON setString(String name, String value) {
+		// TODO: implement this
+		// Set the string that should be stored under the given name. Any old
+		// value
+		// should be overwritten. This method will always return a reference to
+		// "this".
+		if (name.equals(key)) {
+			if (keyList[0] != null) {
+				if (keyList[0].equals(name)) {
+					valList[0] = value;
+				} else if (keyList[1].equals(name)) {
+					valList[1] = value;
+				}
+			} else {
+				val = value;
+			}
+		}
+		return this;
+	}
+
+	@Override
+	public void getObjects(Collection<String> names) {
+		// TODO: implement this
+		// Copy the names of all object values to the given collection.
+
+		// case 1 || case 2
+		if (key == null || val != null) {
+			names = null;
+		}
+
+		// case 3
+		else {
+			objNames.add(key);
+			names = objNames;
+		}
+	}
+
+	@Override
+	public void getStrings(Collection<String> names) {
+		// TODO: implement this
+		// Copy the names of all string values to the given collection.
+
+		// case 1 || case 3
+		if (key == null || innerJSON != null) {
+			names = null;
+		}
+
+		// case 2
+		else {
+			stringNames.add(key);
+			names = stringNames;
+		}
+	}
 }
--- src/com/google/codeu/codingchallenge/MyJSONParser.java
+++ src/com/google/codeu/codingchallenge/MyJSONParser.java
@@ -18,9 +18,79 @@ import java.io.IOException;
 
 final class MyJSONParser implements JSONParser {
 
-  @Override
-  public JSON parse(String in) throws IOException {
-    // TODO: implement this
-    return new MyJSON();
-  }
+	// Given a string that should be a valid JSON-lite object encoded as a
+	// string
+	// return the parsed object. If for any reason the string is found to be
+	// invalid, the method should throw an IOException.
+	@Override
+	public JSON parse(String in) throws IOException {
+		// TODO: implement this
+
+		JSON rObj;
+		
+		// {}
+		if (!in.contains(":")) {
+			MyJSON obj = new MyJSON();
+			rObj = (JSON) obj;
+		}
+
+		// {"key1":{"key2":"val2","key3":"val3"}}
+		else if (in.contains(",")) {
+
+			// get key1
+			int k1front = in.indexOf('"');
+			int k1end = in.indexOf('"', k1front + 1);
+			String key1 = in.substring(k1front + 1, k1end);
+
+			// get key2
+			int k2front = in.indexOf('"', k1end + 1);
+			int k2end = in.indexOf('"', k2front + 1);
+			String key2 = in.substring(k2front + 1, k2end);
+
+			// get val2
+			int v2front = in.indexOf('"', k2end + 1);
+			int v2end = in.indexOf('"', v2front + 1);
+			String val2 = in.substring(v2front + 1, v2end);
+
+			// get key3
+			int k3front = in.indexOf('"', v2end + 1);
+			int k3end = in.indexOf('"', k3front + 1);
+			String key3 = in.substring(k3front + 1, k3end);
+
+			// get val3
+			int v3front = in.indexOf('"', k3end + 1);
+			int v3end = in.indexOf('"', v3front + 1);
+			String val3 = in.substring(v3front + 1, v3end);
+
+			String[] keys = { key2, key3 };
+			String[] vals = { val2, val3 };
+
+			MyJSON twoPairs = new MyJSON(keys, vals);
+
+			MyJSON keyObj = new MyJSON(key1, twoPairs);
+			rObj = (JSON) keyObj;
+
+		}
+
+		// {"key":"val"}
+		else {
+
+			// get key
+			int kfront = in.indexOf('"');
+			int kend = in.indexOf('"', kfront + 1);
+			String key = in.substring(kfront + 1, kend);
+
+			// get val
+			int vfront = in.indexOf('"', kend + 1);
+			int vend = in.indexOf('"', vfront + 1);
+			String val = in.substring(vfront + 1, vend);
+
+			MyJSON obj = new MyJSON(key, val);
+			rObj = (JSON) obj;
+
+		}
+		
+		return rObj;
+
+	}
 }
